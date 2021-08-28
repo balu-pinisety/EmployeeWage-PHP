bfec3da679eec5fa1198d5466d0fe2401cb36b92
Bhuvan Pinisetty <balupinisety@github.com>
2021-08-28 11:02:52 +0530

    Added: Arguments to compute Employee Daily Wage

--- EmpWageBuilder.php
+++ EmpWageBuilder.php
@@ -2,17 +2,22 @@
 class EmpWageBuilder {
     //Constants
 	const IS_FULL_TIME = 1;
+    const WAGE_PER_HOUR = 20;
     public static function computeWage() {
         //Displaying Welcome Message
         echo ("Welcome to Employee Wage Computation Problem\n");
 		//Computation
 		$empCheck = rand(0,1);
 		if ($empCheck == self::IS_FULL_TIME) {
-			echo ("Employee is Present");
+			echo ("Employee is Present\n");
+            $empWorkHrs = 8;
 		}
 		else {
-			echo ("Employee is Absent");
+			echo ("Employee is Absent\n");
+            $empWorkHrs = 0;
 		}
+        $empWage = $empWorkHrs*self::WAGE_PER_HOUR;
+        echo ("Employee Wage: ".$empWage."\n");
     }
 }
 EmpWageBuilder::computeWage();
