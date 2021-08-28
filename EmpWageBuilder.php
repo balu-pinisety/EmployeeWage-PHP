fb98f26be3267a4e1540e479924c2dcf2b69cee7
Bhuvan Pinisetty <balupinisety@github.com>
2021-08-28 10:51:01 +0530

    Added: Method to check Employee Presence

--- EmpWageBuilder.php
+++ EmpWageBuilder.php
@@ -1,8 +1,19 @@
 <?php
 class EmpWageBuilder {
-    function computeWage() {
+    //Constants
+	const IS_FULL_TIME = 1;
+    public static function computeWage() {
         //Displaying Welcome Message
         echo ("Welcome to Employee Wage Computation Problem\n");
+		//Computation
+		$empCheck = rand(0,1);
+		if ($empCheck == self::IS_FULL_TIME) {
+			echo ("Employee is Present");
+		}
+		else {
+			echo ("Employee is Absent");
+		}
     }
 }
+EmpWageBuilder::computeWage();
 ?>
