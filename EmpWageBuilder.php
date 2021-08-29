ab0ed09582388151a61b17d60669107eb0530a51
Bhuvan Pinisetty <balupinisety@github.com>
2021-08-29 20:41:10 +0530

    Modified: Code to clalculate wage for Monthly days of 30

--- EmpWageBuilder.php
+++ EmpWageBuilder.php
@@ -4,27 +4,34 @@ class EmpWageBuilder {
 	const IS_FULL_TIME = 1;
     const IS_PART_TIME = 2;
     const WAGE_PER_HOUR = 20;
+    const MONTH_DAYS = 30;
     public static function computeWage() {
         //Displaying Welcome Message
         echo ("Welcome to Employee Wage Computation Problem\n");
-		//Computation
-		$empCheck = rand(0,2);
-        switch ($empCheck) {
-            case  self::IS_FULL_TIME:
-                echo ("Employee is Present\n");
-                $empWorkHrs = 8;
-                break;
-            case self::IS_PART_TIME:
-                echo ("Employee is Part-time Present\n");
-                $empWorkHrs = 4;
-                break;
-            default:
-                echo ("Employee is Absent\n");
-                $empWorkHrs = 0;
+        $totalWorkHrs = 0;
+        //Calculating wage for Monthly
+		for ($i = 0; $i < self::MONTH_DAYS; $i++) {
+            //Computation
+            $empCheck = rand(0,2);
+            switch ($empCheck) {
+                case  self::IS_FULL_TIME:
+                   // echo ("Employee is Present\n");
+                    $empWorkHrs = 8;
+                    break;
+                case self::IS_PART_TIME:
+                    //echo ("Employee is Part-time Present\n");
+                    $empWorkHrs = 4;
+                    break;
+                default:
+                    //echo ("Employee is Absent\n");
+                    $empWorkHrs = 0;
+            }
+            //Calculating Employee Total Working Hrs
+			$totalWorkHrs+=$empWorkHrs;
         }
         //Calculating Employee Wage
-        $empWage = $empWorkHrs*self::WAGE_PER_HOUR;
-        echo ("Employee Wage: ".$empWage."\n");
+        $empWage = $totalWorkHrs*self::WAGE_PER_HOUR;
+        echo ("Employee Wage: ".$empWage." for working hours of ".$totalWorkHrs."\n");
     }
 }
 EmpWageBuilder::computeWage();
