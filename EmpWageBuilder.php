4d58ca538b885848267c9b33448eeee0850e967a
Bhuvan Pinisetty <balupinisety@github.com>
2021-08-29 20:04:46 +0530

    Added: Part Time Computation

--- EmpWageBuilder.php
+++ EmpWageBuilder.php
@@ -2,6 +2,7 @@
 class EmpWageBuilder {
     //Constants
 	const IS_FULL_TIME = 1;
+    const IS_PART_TIME = 1;
     const WAGE_PER_HOUR = 20;
     public static function computeWage() {
         //Displaying Welcome Message
@@ -11,11 +12,15 @@ class EmpWageBuilder {
 		if ($empCheck == self::IS_FULL_TIME) {
 			echo ("Employee is Present\n");
             $empWorkHrs = 8;
+		} else if ($empCheck == self::IS_PART_TIME) {
+			echo ("Employee is Part-time Present\n");
+            $empWorkHrs = 4;
 		}
 		else {
 			echo ("Employee is Absent\n");
             $empWorkHrs = 0;
 		}
+        //Calculating Employee Wage
         $empWage = $empWorkHrs*self::WAGE_PER_HOUR;
         echo ("Employee Wage: ".$empWage."\n");
     }
